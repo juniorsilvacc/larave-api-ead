58884ed0d78c8976409a63e70c1d8fecd0a82848
juniorsilvacc <juniorsilvacc@hotmail.com>
2023-10-14 02:49:24 -0300

    criando factory de suporte

---
+++ database/factories/SupportFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Lesson;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Support>
+ */
+class SupportFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'description' => fake()->sentence(20),
+            'status' => 'P',
+            'user_id' => User::factory(),
+            'lesson_id' => Lesson::factory(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,6 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        \App\Models\Support::factory(1)->create();
     }
 }
