99313807fd70329538f871ff203efd4c48af1711
juniorsilvacc <juniorsilvacc@hotmail.com>
2023-11-28 13:51:14 -0300

    testes de cursos

---
+++ tests/Feature/Api/Auth/CourseTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Feature\Api\Auth;
+
+use App\Models\Course;
+use Tests\Feature\Api\UtilsTrait;
+use Tests\TestCase;
+
+class CourseTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    use UtilsTrait;
+
+    public function testUnauthenticated(): void
+    {
+        $response = $this->getJson('/courses');
+
+        $response->assertStatus(401);
+    }
+
+    public function testGetCourses(): void
+    {
+        $response = $this->getJson('/courses', $this->defaultHeaders());
+
+        $response->assertStatus(200);
+    }
+
+    public function testGetAllCoursesTotal()
+    {
+        $courses = Course::factory()->count(10)->create();
+
+        $response = $this->getJson('/courses', $this->defaultHeaders());
+
+        $response->assertStatus(200)
+            ->assertJsonCount(count($courses), 'data');
+    }
+
+    public function testGetSingleCourseUnauthenticated()
+    {
+        $response = $this->getJson('/courses/unauthenticated');
+
+        $response->assertStatus(401);
+    }
+
+    public function testGetSingleCourseNotFound()
+    {
+        $response = $this->getJson('/courses/fake_id', $this->defaultHeaders());
+
+        $response->assertStatus(404);
+    }
+
+    public function testGetSingleCourse()
+    {
+        $course = Course::factory()->create();
+
+        $response = $this->getJson("/courses/{$course->id}", $this->defaultHeaders());
+
+        $response->assertStatus(200);
+    }
+}
--- tests/Feature/Api/UtilsTrait.php
+++ tests/Feature/Api/UtilsTrait.php
@@ -24,4 +24,13 @@ trait UtilsTrait
 
         return $token;
     }
+
+    public function defaultHeaders()
+    {
+        $token = $this->createTokenUser();
+
+        return [
+            'Authorization' => "Bearer {$token}",
+        ];
+    }
 }
